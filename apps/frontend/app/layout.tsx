9c0e792fbfbf5780cb883287978b42b5f90f7ade
ashoomky <ashlee@shum.co.nz>
2026-08-20 23:24:52 +1200

    refactor: switch Satoshi font to variable font

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -6,16 +6,8 @@ import { cn } from "@repo/ui/lib/utils"
 
 const satoshi = localFont({
   src: [
-    { path: "./fonts/Satoshi-Light.otf", weight: "300", style: "normal" },
-    { path: "./fonts/Satoshi-LightItalic.otf", weight: "300", style: "italic" },
-    { path: "./fonts/Satoshi-Regular.otf", weight: "400", style: "normal" },
-    { path: "./fonts/Satoshi-Italic.otf", weight: "400", style: "italic" },
-    { path: "./fonts/Satoshi-Medium.otf", weight: "500", style: "normal" },
-    { path: "./fonts/Satoshi-MediumItalic.otf", weight: "500", style: "italic" },
-    { path: "./fonts/Satoshi-Bold.otf", weight: "700", style: "normal" },
-    { path: "./fonts/Satoshi-BoldItalic.otf", weight: "700", style: "italic" },
-    { path: "./fonts/Satoshi-Black.otf", weight: "900", style: "normal" },
-    { path: "./fonts/Satoshi-BlackItalic.otf", weight: "900", style: "italic" },
+    { path: "./fonts/Satoshi-Variable.ttf", weight: "300 900", style: "normal" },
+    { path: "./fonts/Satoshi-VariableItalic.ttf", weight: "300 900", style: "italic" },
   ],
   variable: "--font-sans",
 })
