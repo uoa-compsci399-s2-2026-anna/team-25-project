46b4a5b0fe8cac3da12d6bf1ddac5562c6399e59
ashoomky <ashlee@shum.co.nz>
2026-08-20 13:40:08 +1200

    feat: define colours and fonts

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,9 +1,24 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist_Mono } from "next/font/google"
+import localFont from "next/font/local"
 
 import "@repo/ui/globals.css"
 import { cn } from "@repo/ui/lib/utils"
 
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+const satoshi = localFont({
+  src: [
+    { path: "./fonts/Satoshi-Light.otf", weight: "300", style: "normal" },
+    { path: "./fonts/Satoshi-LightItalic.otf", weight: "300", style: "italic" },
+    { path: "./fonts/Satoshi-Regular.otf", weight: "400", style: "normal" },
+    { path: "./fonts/Satoshi-Italic.otf", weight: "400", style: "italic" },
+    { path: "./fonts/Satoshi-Medium.otf", weight: "500", style: "normal" },
+    { path: "./fonts/Satoshi-MediumItalic.otf", weight: "500", style: "italic" },
+    { path: "./fonts/Satoshi-Bold.otf", weight: "700", style: "normal" },
+    { path: "./fonts/Satoshi-BoldItalic.otf", weight: "700", style: "italic" },
+    { path: "./fonts/Satoshi-Black.otf", weight: "900", style: "normal" },
+    { path: "./fonts/Satoshi-BlackItalic.otf", weight: "900", style: "italic" },
+  ],
+  variable: "--font-sans",
+})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -16,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)} lang="en">
+    <html className={cn("antialiased", fontMono.variable, "font-sans", satoshi.variable)} lang="en">
       <body>{children}</body>
     </html>
   )
