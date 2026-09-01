e10c6507c1e3bdf203d75a10ddb0fe3c56fec922
mikais13 <mikai.somerville@gmail.com>
2026-09-01 20:23:28 +1200

    fix(skeleton): add aria-hidden true

--- packages/ui/src/components/ui/Skeleton/skeleton.tsx
+++ packages/ui/src/components/ui/Skeleton/skeleton.tsx
@@ -3,6 +3,7 @@ import { cn } from "@repo/ui/lib/utils"
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
+      aria-hidden="true"
       className={cn("animate-pulse rounded-md bg-muted", className)}
       data-slot="skeleton"
       {...props}
