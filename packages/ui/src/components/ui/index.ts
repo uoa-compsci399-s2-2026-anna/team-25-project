3c79d33d2dd7961f09c279e0f979276774a5f0bb
Andre Camerino <camerinoandre@gmail.com>
2026-09-05 21:57:47 +1200

    feat(ui): add Checkbox component

--- packages/ui/src/components/ui/index.ts
+++ packages/ui/src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from "./Avatar/avatar"
 export * from "./Badge/badge"
 export * from "./Button/button"
+export * from "./Checkbox/checkbox"
 export * from "./Heading/heading"
 export * from "./Input/input"
 export * from "./Skeleton/skeleton"
